4dbdb84439fdad99bd502eae9e05d7bb283b5a20
Jack Matthews <jackhkmatthews@gmail.com>
2017-07-26 18:54:13 +0100

    preper use of model promise chaining

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,40 +1,34 @@
 const models = require('../models/index');
 
-console.log(models);
+let dayID;
+let staffID;
 
-models.StaffMember.create({
+models.StaffMember
+.create({
   firstName: 'Jack',
   lastName: 'Matthews',
   role: 'Developer'
 }, {
   fields: ['firstName', 'lastName', 'role']
-}).then(instertedStaff => {
-  console.log(instertedStaff.dataValues);
-});
-
-models.Day.create({
+})
+.then(models.Day.create({
   date: Date.now()
 }, {
   fields: ['date']
-}).then(instertedDays => {
-  console.log(instertedDays.dataValues);
-});
-
-let dayID;
-let staffID;
-
-models.Day.findAll().then(days => {
+}))
+.then(() => models.Day.findAll())
+.then(days => {
   dayID = days[0].ID;
-  models.StaffMember.findAll().then(staffMembers => {
-    staffID = staffMembers[0].ID;
-    models.Summary.create({
-      orderIndex: 1,
-      dayID: dayID,
-      staffID: staffID
-    }).then(insertedSummary => {
-      console.log(insertedSummary);
-    });
+  return models.StaffMember.findAll();
+})
+.then(staffMembers => {
+  staffID = staffMembers[0].ID;
+  return models.Summary.create({
+    orderIndex: 1,
+    dayID: dayID,
+    staffID: staffID
   });
+})
+.then(insertedSummary => {
+  console.log(insertedSummary);
 });
-
-
