512cf60e2d294aedeb3cba3077a0acbf15a03958
Jack Matthews <jackhkmatthews@gmail.com>
2017-08-04 14:26:47 +0100

    Loggin errors as they occur and then passing on

--- seeders/staff-members.js
+++ seeders/staff-members.js
@@ -7,30 +7,32 @@ const staffMembers = require('./staff-members.json');
 const logger = new Logger();
 logger.debugLevel = 'success';
 
-function saveStaffMembers(staffMembers){
-  const promises = staffMembers.map(staffMember => {
-    return models.StaffMember.create(staffMember)
-      .catch(err => err);
-  });
-  return Promise.all(promises);
-}
-
-function logStaffMemberObject(staffMemberObject, index) {
-  if (staffMemberObject.errors) {
-    return logger.log('error', `${staffMemberObject.errors[0].message}. StaffMember ${++index} was not saved.`);
-  }
-  logger.log('success', `${staffMemberObject.dataValues.fullName} was saved.`);
-}
-
-sequelize.query('DELETE FROM "StaffMember"')
+sequelize.query('DELETE FROM "Position"')
+  .then(sequelize.query('DELETE FROM "Day"'))
+  .then(sequelize.query('DELETE FROM "Summary"'))
+  .then(sequelize.query('DELETE FROM "StaffMember"'))
   .then(() => saveStaffMembers(staffMembers))
-  .then(staffMemberObjects => {
-    staffMemberObjects.forEach((staffMemberObject, index) => {
-      logStaffMemberObject(staffMemberObject, index);
-    });
-    process.exit();
-  })
+  .then(() => process.exit())
   .catch(err => {
     logger.log('error', err);
     process.exit();
   });
+
+function saveStaffMembers(staffMembers){
+  const promises = staffMembers.map((staffMember, index) => {
+    return saveStaffMember(staffMember, index);
+  });
+  return Promise.all(promises);
+}
+
+function saveStaffMember(staffMember, index) {
+  return models.StaffMember.create(staffMember)
+    .then(staffMemberObject => {
+      logger.log('success', `${staffMemberObject.dataValues.fullName} was saved.`);
+      return staffMemberObject.dataValues;
+    })
+    .catch(err => {
+      logger.log('error', `${err.errors[0].message}. StaffMember ${++index} was not saved.`);
+      return err.errors;
+    });
+}
