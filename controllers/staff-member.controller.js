12c650c75d51971e5fd5da0f76dd96290b14dff9
Jack Matthews <jackhkmatthews@gmail.com>
2017-08-07 15:10:26 +0100

    staff member docs routes and controller

--- controllers/staff-member.controller.js
+++ controllers/staff-member.controller.js
@@ -1,7 +1,87 @@
-exports.find = function get(req, res) {
-  res.json([{ reasource: 'staffMembers' }]);
-};
+const hal = require('hal');
+const Logger = require('logger').Logger;
+
+const models = require('../models');
+const logger = new Logger();
+logger.debugLevel = 'warn';
+
+const root = 'v1';
+
+class StaffMember extends hal.Resource {
+  constructor(data) {
+    delete data.createdAt;
+    delete data.updatedAt;
+    super(data, `${root}/staff-member/${data.ID}`);
+  }
+}
+
+class StaffMembersList extends hal.Resource {
+  constructor(url, staffMembers) {
+    super({}, url);
+    if (staffMembers.length) this.embed('staffMembers', staffMembers);
+  }
+}
 
 exports.get = function get(req, res) {
-  res.json([{ reasource: 'staffMember' }]);
+  models.StaffMember.findById(req.params.id)
+    .then(staffMember => {
+      const resource = new StaffMember(staffMember.dataValues);
+      res.status(200).json(resource);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 };
+
+exports.create = function create(req, res) {
+  models.StaffMember.create(req.body)
+    .then(staffMember => {
+      const resource = new StaffMember(staffMember.dataValues);
+      res.status(200).json(resource);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
+
+exports.find = function find(req, res) {
+  models.StaffMember.findAll()
+    .then(staffMembers => {
+      staffMembers = staffMembers.map(staffMember => {
+        return new StaffMember(staffMember.dataValues);
+      });
+      const resource = new StaffMembersList(req.originalUrl, staffMembers);
+      res.json(resource);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
+
+exports.update = function update(req, res) {
+  models.StaffMember.findById(req.params.id)
+    .then(staffMember => {
+      return staffMember.update(req.body);
+    })
+    .then(staffMember => {
+      const resource = new StaffMember(staffMember.dataValues);
+      res.status(200).json(resource);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
+
+exports.delete = (req, res) => {
+  models.StaffMember.findById(req.params.id)
+    .then(staffMember => {
+      return staffMember.destroy();
+    })
+    .then(noDestroyed => {
+      res.status(200).json(noDestroyed);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
+
--- routes/staff-members.js
+++ routes/staff-members.js
@@ -6,7 +6,13 @@ const router = new express.Router();
 const staffMemberCtrl = require('../controllers').staffMemberCtrl;
 
 // Routes
-router.get('/', staffMemberCtrl.find);
-router.get('/:id', staffMemberCtrl.get);
+router.route('/')
+  .get(staffMemberCtrl.find)
+  .post(staffMemberCtrl.create);
+
+router.route('/:id')
+  .get(staffMemberCtrl.get)
+  .put(staffMemberCtrl.update)
+  .delete(staffMemberCtrl.delete);
 
 module.exports = router;
