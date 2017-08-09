bdd8e6d557c387babd5fd71e00be83b6ed7c7f53
Jack Matthews <jackhkmatthews@gmail.com>
2017-08-09 19:01:22 +0100

    Standup endpoint

--- controllers/stand-up.js
+++ controllers/stand-up.js
@@ -1,16 +1,18 @@
 const hal = require('hal');
 
 const models = require('../models');
-const httpPromise = require('../test');
+const Day = require('../resources').Day;
+const DaysList = require('../resources').DaysList;
+const Position = require('../resources').Position;
+const PositionsList = require('../resources').PositionsList;
+const Summary = require('../resources').Summary;
+const SummariesList = require('../resources').SummariesList;
 
 const root = 'v1';
 
-class StandUp extends hal.Resource {
+class StandUp {
   constructor(day, positions, summaries) {
-    super({}, `${root}/standUps`);
-    this.embed('day', day);
-    this.embed('positions', positions);
-    this.embed('summaries', summaries);
+    return {day, positions, summaries};
   }
 }
 
@@ -22,24 +24,38 @@ class StandUpsList extends hal.Resource {
 }
 
 exports.get = function get(req, res) {
-  httpPromise('http://localhost:3000/staff-members').then(value => console.log('outside', value));
+
+  const ID = req.query.dayID;
+  
+  const day = models.Day.findAll({ where: {ID} })
+    .then(days => {
+      const resource = new Day(days[0].dataValues);
+      return resource;
+    });
+  const positions = models.Position.findAll({ where: req.query })
+    .then(positions => {
+      positions = positions.map(position => {
+        return new Position(position.dataValues);
+      });
+      const resource = new PositionsList(req.originalUrl, positions);
+      return resource;
+    });
+  const summaries = models.Summary.findAll({ where: req.query })
+    .then(summaries => {
+      summaries = summaries.map(summary => {
+        return new Summary(summary.dataValues);
+      });
+      const resource = new SummariesList(req.originalUrl, summaries);
+      return resource;
+    });
+
+  Promise.all([day, positions, summaries])
+    .then(results => {
+      const [day, positions, summaries] = results;
+      console.log(day);
+      const resource = new StandUp(day, positions, summaries);
+      console.log(resource);
+      res.status(200).json(resource);
+    });
 };
 
-
-/*
-  http days
-  .then
-    map
-      used day ID to make request to positions DB
-        strip out unused bits
-      use day ID to make request to summaries
-        strip out unused bits
-      return promise.all[day, positions, summaries]
-      .then
-        return construct standUp reasource with class (day, position, summaries)
-  .then
-    construct standUpList with class (url, standUps)
-    res.status standUpListResource
-
-
-*/
--- routes/stand-ups.js
+++ routes/stand-ups.js
@@ -4,8 +4,8 @@ const router = new express.Router();
 const standUpCtrl = require('../controllers').standUpCtrl;
 
 // Routes
-// router.route('/')
-//   .get(standUpCtrl.get)
+router.route('/')
+  .get(standUpCtrl.get);
 //   .post(standUpCtrl.create);
 
 module.exports = router;
