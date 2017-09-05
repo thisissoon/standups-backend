728a4337a52ae3def2adbb3ffa4a24cbf3551afa
Jack Matthews <jackhkmatthews@gmail.com>
2017-09-05 14:59:51 +0100

    circleCI beginnings

--- test/main_test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-
-// Test suite
-describe('Mocha', function () {
-  //Test spec / unit test
-  it('should run our tests using npm', function () {
-    expect(true).to.be.ok;
-  });
-});
