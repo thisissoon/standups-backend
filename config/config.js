e316953c874df9b104eeb7fae957872d3724895a
Jack Matthews <jackhkmatthews@gmail.com>
2017-09-11 14:51:46 +0100

    syntax

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-module.exports = {
+const config = {
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.TEST_DB_NAME || process.env.DB_NAME || 'stand-ups-dev',
+  database: process.env.TEST_DB_NAME || process.env.DB_NAME || 'stand-ups-test',
   host: process.env.DB_HOST || '127.0.0.1',
   dialect: 'postgres',
   logging: process.env.DB_LOGGING || false,
@@ -10,3 +10,5 @@ module.exports = {
   serverPort: process.env.SERVER_PORT || 3000,
   testDb: process.env.TEST_DB_NAME || 'stand-ups-test'
 };
+
+module.exports = config;
