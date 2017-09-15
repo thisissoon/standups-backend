b0d9802acd243159efc81d504c8ceb8940ce7509
Jack Matthews <jackhkmatthews@gmail.com>
2017-09-15 12:26:25 +0100

    App will try to connect to DB 5 times. Docker-compose related.

--- db/connection.js
+++ db/connection.js
@@ -3,15 +3,27 @@ const config = require('../config/config');
 
 const sequelize = new Sequelize(config);
 
-sequelize
+function connect(counter) {
+  sequelize
   .authenticate()
   .then(() => {
     console.log(`Connection to ${config.database} has been established successfully.`);
   })
   .catch(err => {
-    console.error(`Unable to connect to ${config.database}:`, err);
+    console.error(`Unable to connect to ${config.database}:`, err.original);
+    console.log('attempt: ', counter);
+    if (counter < 5) {
+      console.log('re-trying');
+      setTimeout(() => {
+        connect(++counter);
+      }, 5000);
+    } else {
+      console.log('maximum re-trys exceeded');
+    }
   });
+  module.exports = {
+    sequelize
+  };
+}
 
-module.exports = {
-  sequelize
-};
+connect(1);
